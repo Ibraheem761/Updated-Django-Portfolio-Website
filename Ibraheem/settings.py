685f37ebcfa3be6f88073d399499e7cc29fc5f09
unknown <elansariibraheem@gmail.com>
2021-08-24 01:49:02 +0100

    static root 8

--- Ibraheem/settings.py
+++ Ibraheem/settings.py
@@ -140,9 +140,14 @@ STATICFILES_DIRS = [
        ]
 
 import sys
+
 if sys.argv[1] != 'runserver':
     STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
+else:
+    STATICFILES_DIRS = [
+                os.path.join(BASE_DIR, 'static')
+        ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
