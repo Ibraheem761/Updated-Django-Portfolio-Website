de25a3c5cf3f94abcbf4165c696d79f7b8392316
unknown <elansariibraheem@gmail.com>
2021-08-24 01:42:26 +0100

    static root 4

--- Ibraheem/settings.py
+++ Ibraheem/settings.py
@@ -134,12 +134,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 
-if DEBUG:
-        STATICFILES_DIRS = [
+
+STATICFILES_DIRS = [
             os.path.join(BASE_DIR, 'static')
        ]
-else:
-        STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 
 
