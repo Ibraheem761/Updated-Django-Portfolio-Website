b563f578325dc85e276647fc2f2f5dfbe688f90c
unknown <elansariibraheem@gmail.com>
2021-08-24 01:21:06 +0100

    add db

--- Ibraheem/settings.py
+++ Ibraheem/settings.py
@@ -76,13 +76,24 @@ WSGI_APPLICATION = 'Ibraheem.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'portfolio',
+            'USER': 'ibraheem',
+            'PASSWORD': 'meeharbi',
+            'HOST': 'localhost',
+            'PORT': '',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
