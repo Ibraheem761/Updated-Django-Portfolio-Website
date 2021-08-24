d736d633096b32d1a32ac08327b71027a47842d7
unknown <elansariibraheem@gmail.com>
2021-08-24 01:56:23 +0100

    static root 10

--- Ibraheem/settings.py
+++ Ibraheem/settings.py
@@ -134,15 +134,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 
-import sys
 
-if sys.argv[1] != 'runserver':
-    STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-else:
-    STATICFILES_DIRS = [
-                os.path.join(BASE_DIR, 'static')
-        ]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
